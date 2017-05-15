6fa478f1d143e625ea3b1a2caed37f5cc006e80c
Jacek Pietras <jaxilus@gmail.com>
2017-05-15 21:39:21 +0200

    Porzątkowanie kodu

--- src/main/java/com/rekrutacja/netpc/account/service/SecurityService.java
+++ src/main/java/com/rekrutacja/netpc/account/service/SecurityService.java
@@ -1,7 +1,7 @@
 package com.rekrutacja.netpc.account.service;
 
 public interface SecurityService {
-    String findLoggedInUsername();
+	String findLoggedInUsername();
 
-    void autologin(String username, String password);
+	void autologin(String username, String password);
 }
